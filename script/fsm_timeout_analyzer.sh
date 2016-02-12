d4aef16a1d0c1e780ba553cb86420df4839e7178
David Cromberge <david.cromberge@dataloop.io>
2016-02-12 13:52:09 +0000

    add time distribution

--- script/fsm_timeout_analyzer.sh
+++ script/fsm_timeout_analyzer.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 LOG=$1
 
-echo -e "analyzing $LOG...: \n"
+echo -e "Analyzing $LOG...: \n"
 
 ### NUMBER OF TIMEOUTS ###
 echo -e "\nNumber of timeouts:\n==="
-grep "exception exit: {timeout" $LOG | wc -l
+grep "Reason:     timeout" $LOG | wc -l
+
 ## BUCKET INFO
 echo -e "\nDifferent buckets:\n==="
 grep "Data  ==" $LOG | colrm 100 | awk -F',' '{print $4}' | sort | uniq -c | sort
+
+## TIME DIST
+echo -e "\nTime distribution:\n==="
+grep "ERROR REPORT====$" $LOG | awk '{print $1 " " $2}' | sort | uniq -c
